03eb7621425563168fcfa05b674cebe8ab1a0136
Shaun Sharples <shaun.sharples@gmail.com>
2023-01-13 13:38:15 +0200

    new thread for each poller rather than scheduler

--- lib/Tuya/Poll.hs
+++ lib/Tuya/Poll.hs
@@ -7,13 +7,12 @@ import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Exception
 import Control.Monad
-import Crypto.Error (CryptoError)
 import Data.Aeson
 import qualified Data.Aeson.Key as Key
 import qualified Data.Aeson.KeyMap as KeyMap
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Fixed as Fixed
+import Data.Foldable (traverse_)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.IORef
@@ -34,6 +33,7 @@ import Tuya.Types
 data Env = Env
   { envCfg :: Config
   , envMc :: MQTT.MQTTClient
+  , envPollers :: IORef (HashMap Text (Async ()))
   , envIps :: IORef (HashMap Text Text)
   , envKeys :: IORef (HashMap Text BS.ByteString)
   , envSpecs :: IORef (HashMap Text Specification)
@@ -44,78 +44,8 @@ data Env = Env
 poller :: Config -> IO ()
 poller cfg = do
   mc <- MQTT.connectURI MQTT.mqttConfig{MQTT._protocol = mqttProtocol (cfgMqtt cfg)} (mqttBrokerUri (cfgMqtt cfg))
-  env <- Env cfg mc <$> newIORef mempty <*> newIORef mempty <*> newIORef mempty <*> newIORef mempty <*> newIORef mempty
-  concurrently_ (pollDevices env) (sub env)
-
-pollDevices :: Env -> IO ()
-pollDevices env = do
-  threadDelay 1000000
-  forever $ do
-    now <- getCurrentTime
-    next <- atomicModifyIORef' (envSchedule env) $ \q ->
-      case PQ.getMin q of
-        Just (t, m) -> do
-          if now < t
-            then (q, Left $ min (micro $ diffUTCTime t now) 5000000)
-            else (PQ.deleteMin q, Right m)
-        Nothing -> (q, Left 2000000)
-    case next of
-      Right devId -> do
-        n <- pollDevice env devId
-        case n of
-          Just t -> schedule devId t (envSchedule env)
-          Nothing -> return ()
-      Left d -> threadDelay d
-
-micro :: NominalDiffTime -> Int
-micro d = let Fixed.MkFixed f = nominalDiffTimeToSeconds d in fromIntegral (f `div` 1000000)
-
-pollDevice :: Env -> Text -> IO (Maybe NominalDiffTime)
-pollDevice env devId = do
-  ips <- readIORef (envIps env)
-  keys <- readIORef (envKeys env)
-  case (HM.lookup devId ips, HM.lookup devId keys) of
-    (Just ip, Just key) ->
-      -- handle (\e -> print (e :: IOException) >> return (Just 0)) $ do
-      catches
-        (go ip key)
-        [ Handler
-            (\e -> print (e :: IOException) >> return (Just 0))
-        , Handler
-            (\e -> print (e :: CryptoError) >> return (Just 0))
-        ]
-    _ -> return (Just 60)
- where
-  getT' = Text.pack . formatTime defaultTimeLocale "%s" <$> getCurrentTime
-  go ip key = do
-    sockaddr <- sockAddrForIp ip
-    bracket (connect sockaddr Tuya33 key) close $ \c -> do
-      t <- getT'
-      sendCmd c DpQuery (GetDeviceStatus devId devId t devId)
-      v <- recvMsg c
-      case v of
-        Left _ -> return ()
-        Right msg -> deviceStatus env devId (msgPayload msg)
-    return (Just 10)
-
-deviceStatus :: Env -> Text -> DeviceStatus -> IO ()
-deviceStatus env devId status = do
-  maps <- readIORef (envStatusMap env)
-  case HM.lookup devId maps of
-    Just sm ->
-      forM_ (KeyMap.toList (sDps status)) $ \(dp, val) -> do
-        case HM.lookup (Key.toText dp) sm of
-          Just code -> do
-            let Just topic = MQTT.mkTopic ("tuya/device/" <> devId <> "/status/" <> code)
-            MQTT.publish (envMc env) topic (encode val) True
-          Nothing -> return ()
-    Nothing -> return ()
-
-sockAddrForIp :: Text -> IO S.SockAddr
-sockAddrForIp ip = do
-  let hints = S.defaultHints{S.addrFlags = [S.AI_NUMERICHOST], S.addrSocketType = S.Stream}
-  addr : _ <- S.getAddrInfo (Just hints) (Just $ Text.unpack ip) (Just "6668")
-  return (S.addrAddress addr)
+  env <- Env cfg mc <$> newIORef mempty <*> newIORef mempty <*> newIORef mempty <*> newIORef mempty <*> newIORef mempty <*> newIORef mempty
+  sub env
 
 sub :: Env -> IO ()
 sub env = do
@@ -138,12 +68,12 @@ msgReceived :: Env -> MQTT.MQTTClient -> MQTT.Topic -> LBS.ByteString -> [MQTT.P
 msgReceived env _mc topic payload _
   | MQTT.match "tuya/device/+/ip" topic = do
       let devId = MQTT.unTopic $ MQTT.split topic !! 2
-      ips <- readIORef (envIps env)
-      unless (HM.member devId ips) (schedule devId 1 (envSchedule env))
+      cancelPoller env devId
       update
         devId
         (decodeUtf8 $ LBS.toStrict payload)
         (envIps env)
+      startPoller env devId
   | MQTT.match "tuya/device/+/key" topic =
       update
         (MQTT.unTopic $ MQTT.split topic !! 2)
@@ -164,15 +94,46 @@ msgReceived env _mc topic payload _
 update :: Text -> v -> IORef (HashMap Text v) -> IO ()
 update devId val ref = modifyIORef' ref (HM.insert devId val)
 
-schedule :: Text -> NominalDiffTime -> IORef (PQ.MinPQueue UTCTime Text) -> IO ()
-schedule devId n ref = do
-  t <- getCurrentTime
-  modifyIORef' ref $ PQ.insert (addUTCTime n t) devId
-
 statusMap :: Specification -> HashMap Text Text
 statusMap spec = mconcat $ map go (specStatus spec)
  where
   go s = HM.singleton (Text.pack $ show $ statusDpId s) (statusCode s)
+
+startPoller :: Env -> Text -> IO ()
+startPoller env devId = do
+  a <- async (devicePoller env devId)
+  modifyIORef' (envPollers env) $ HM.insert devId a
+
+cancelPoller :: Env -> Text -> IO ()
+cancelPoller env devId = do
+  a <- atomicModifyIORef' (envPollers env) $ \pollers ->
+    case HM.lookup devId pollers of
+      Just a -> (HM.delete devId pollers, Just a)
+      Nothing -> (pollers, Nothing)
+  traverse_ cancel a
+
+devicePoller :: Env -> Text -> IO ()
+devicePoller env devId = forever $ pollDevice env devId
+
+pollDevice :: Env -> Text -> IO ()
+pollDevice env devId = do
+  ips <- readIORef (envIps env)
+  keys <- readIORef (envKeys env)
+  maps <- readIORef (envStatusMap env)
+  case (HM.lookup devId ips, HM.lookup devId keys, HM.lookup devId maps) of
+    (Just ip, Just key, Just smap) -> do
+      sockaddr <- sockAddrForIp ip
+      bracket (connect sockaddr Tuya33 key) close $ \c -> do
+        t <- getT'
+        sendCmd c DpQuery (GetDeviceStatus devId devId t devId)
+        v <- recvMsg c
+        case v of
+          Left err -> error $ "recvMesg failed: " <> err
+          Right msg -> deviceStatus env devId smap (msgPayload msg)
+      threadDelay 10000000
+    _ -> threadDelay 10000000
+ where
+  getT' = Text.pack . formatTime defaultTimeLocale "%s" <$> getCurrentTime
 
 data GetDeviceStatus = GetDeviceStatus
   { gdsGwId :: Text
@@ -199,3 +160,18 @@ instance FromJSON DeviceStatus where
   parseJSON =
     withObject "DeviceStatus" $ \o ->
       DeviceStatus <$> o .: "dps"
+
+deviceStatus :: Env -> Text -> HashMap Text Text -> DeviceStatus -> IO ()
+deviceStatus env devId smap status = do
+  forM_ (KeyMap.toList (sDps status)) $ \(dp, val) -> do
+    case HM.lookup (Key.toText dp) smap of
+      Just code -> do
+        let Just topic = MQTT.mkTopic ("tuya/device/" <> devId <> "/status/" <> code)
+        MQTT.publish (envMc env) topic (encode val) True
+      Nothing -> return ()
+
+sockAddrForIp :: Text -> IO S.SockAddr
+sockAddrForIp ip = do
+  let hints = S.defaultHints{S.addrFlags = [S.AI_NUMERICHOST], S.addrSocketType = S.Stream}
+  addr : _ <- S.getAddrInfo (Just hints) (Just $ Text.unpack ip) (Just "6668")
+  return (S.addrAddress addr)
