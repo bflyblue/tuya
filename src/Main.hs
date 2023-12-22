b3484d5b381f6f0c53e24e95b65e82d6f438aa70
Shaun Sharples <shaun.sharples@gmail.com>
2023-12-22 03:27:17 +0200

    poller

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,10 @@
 
 module Main where
 
-import Control.Concurrent.Async
-
 import Tuya.Config
-import Tuya.Devices
-import Tuya.Discover
-import Tuya.HomeAssistant
 import Tuya.Poll
 
 main :: IO ()
 main = do
   cfg <- readConfigFile "tuya.yaml"
   poller cfg
-
--- discover (cfgMqtt cfg)
---   `concurrently_` serve cfg
---   `concurrently_` poller cfg
---   `concurrently_` homeAssist cfg
