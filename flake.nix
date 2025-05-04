1de5426f287a898ee76b5daaeb573fd4c12ebe75
Shaun Sharples <shaun.sharples@gmail.com>
2025-05-04 13:57:22 +0200

    ghc910

--- flake.nix
+++ flake.nix
@@ -2,47 +2,67 @@
   description = "tuya";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        hlib = pkgs.haskell.lib;
-        haskellPackages = pkgs.haskell.packages.ghc98.override {
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forallSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f (rec {
+            inherit system;
+            pkgs = nixpkgsFor system;
+            haskellPackages = hpkgsFor system pkgs;
+          })
+        );
+      nixpkgsFor = system: import nixpkgs { inherit system; };
+      hpkgsFor =
+        system: pkgs:
+        with pkgs.haskell.lib;
+        pkgs.haskell.packages.ghc910.override {
           overrides = self: super: {
-            net-mqtt = hlib.doJailbreak super.net-mqtt;
+            net-mqtt = doJailbreak super.net-mqtt;
+            postgresql-libpq = doJailbreak super.postgresql-libpq;
+            websockets = doJailbreak super.websockets;
           };
-
         };
-        packageName = "tuya";
-      in
-      {
-        packages.${packageName} = haskellPackages.callCabal2nix packageName self { };
-
-        packages.default = self.packages.${system}.${packageName};
-
-        devShells.default = haskellPackages.shellFor {
-          packages = p: [ self.packages.${system}.${packageName} ];
-          buildInputs = with haskellPackages; [
-            cabal-install
-            haskell-language-server
-          ];
-          withHoogle = true;
-        };
-      }
-    );
+    in
+    {
+      packages = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          tuya = haskellPackages.callCabal2nix "tuya" ./. { };
+          default = self.packages.${system}.tuya;
+        }
+      );
+      devShells = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          tuya = haskellPackages.shellFor {
+            packages = p: [ self.packages.${system}.tuya ];
+            buildInputs = with haskellPackages; [
+              cabal-install
+              haskell-language-server
+            ];
+            withHoogle = true;
+          };
+          default = self.devShells.${system}.tuya;
+        }
+      );
+    };
 }
