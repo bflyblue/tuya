e5f4d0e9c0bbcecff1681c3f741c1010f4f7d61e
Shaun Sharples <shaun.sharples@gmail.com>
2025-05-19 09:57:06 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -5,40 +5,60 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
-      forallSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
+      forallSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f (rec {
             inherit system;
             pkgs = nixpkgsFor system;
             haskellPackages = hpkgsFor system pkgs;
-          }));
+          })
+        );
       nixpkgsFor = system: import nixpkgs { inherit system; };
-      hpkgsFor = system: pkgs:
+      hpkgsFor =
+        system: pkgs:
         with pkgs.haskell.lib;
         pkgs.haskell.packages.ghc96.override {
           overrides = self: super: {
-            pqueue = super.pqueue_1_5_0_0;
-            req = doJailbreak super.req;
+            # pqueue = super.pqueue_1_5_0_0;
+            # req = doJailbreak super.req;
           };
         };
-    in {
-      packages = forallSystems ({ system, pkgs, haskellPackages }: {
-        tuya = haskellPackages.callCabal2nixWithOptions "tuya" ./. "-fdebug" { };
-        default = self.packages.${system}.tuya;
-      });
-      devShells = forallSystems ({ system, pkgs, haskellPackages }: {
-        tuya = haskellPackages.shellFor {
-          packages = p: [ self.packages.${system}.tuya ];
-          buildInputs = with haskellPackages; [
-            cabal-install
-            haskell-language-server
-          ];
-          withHoogle = true;
-        };
-        default = self.devShells.${system}.tuya;
-      });
+    in
+    {
+      packages = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          tuya = haskellPackages.callCabal2nixWithOptions "tuya" ./. "-fdebug" { };
+          default = self.packages.${system}.tuya;
+        }
+      );
+      devShells = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          tuya = haskellPackages.shellFor {
+            packages = p: [ self.packages.${system}.tuya ];
+            buildInputs = with haskellPackages; [
+              cabal-install
+              haskell-language-server
+            ];
+            withHoogle = true;
+          };
+          default = self.devShells.${system}.tuya;
+        }
+      );
     };
 }
